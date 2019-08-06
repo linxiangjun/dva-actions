b491e4b7b406584046decda4b5d99b98e46b7cbb
linxiangjun <xiangjun.lin@outlook.com>
2019-08-06 21:24:09 +0800

    actions

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const actions = (fc, models) => {
+  const actions = {};
+  const { g_app } = window;
+  const { _store } = g_app || {};
+  const { dispatch } = _store || {};
+
+  if (!dispatch) {
+    throw "window has no dispatch methods!";
+  }
+
+  Object.keys(models).forEach(
+    name =>
+      Array.isArray(models[name]) &&
+      models[name].forEach(effect => {
+        const type = `${name}/${effect}`;
+        const fn = payload =>
+          new Promise((resolve, reject) =>
+            dispatch({
+              type,
+              payload
+            })
+              .then(data => {
+                resolve(data);
+              })
+              .catch(err => {
+                reject(err);
+              })
+          );
+
+        actions[effect] = fn;
+      })
+  );
+
+  return props => {
+    const newProps = {
+      ...props,
+      actions
+    };
+    return fc(newProps);
+  };
+};
+
+export default actions;
